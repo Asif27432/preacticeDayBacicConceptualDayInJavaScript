0db08a3b99f6898a9d21057920f151bc870fca50
Asif27432 <muhammadasif2m@gmail.com>
2022-02-03 13:49:17 +0600

    first commit

---
+++ factorial.js
@@ -0,0 +1,11 @@
+
+function factorial(n) {
+    var fact = 1;
+    for (var i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+var result = factorial(6);
+console.log(result);
---
+++ variable-name.js
@@ -0,0 +1,26 @@
+/*var name = 'Hero Alan';
+
+// reserved keyword not allowed
+var false = 15;
+var delete = 'delete';
+
+// name should be one word
+var nayok name = 'Tom HANKS'; // NAMER MAJHKHANE KONO SPACE DEWA JABE NH
+
+// quote is not allowed
+var "nayika" = "shabnoor";
+// namer moddhe kono kotesition dewa jabo na... colon chinno dewa jabe nh
+
+var my-office - address = 'andorkilla bandorban';
+var my_office_address = 'andorkilla bandorban';
+var myOfficeAddress = 'andorkilla bandorban';
+var myofficeaddress = 'andorkilla bandorban';
+
+
+var PIE = 3.14;
+
+var 999price = 25; //Nammer age kokhono numbr dite parbo na.
+var p45 = 96; // namer pore number dite hobe
+
+var userHomeAddress = 'katabon';
+var uha = 'katabon';*/
